bc335461f878ecea3df83ebd247f254e9e867ca5
Théo DURR <git@theodurr.fr>
2022-11-17 16:59:03 +0100

    Implemented follow mechanic and logic

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,9 +7,9 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo>
-                           <x-slot:styles>
-                               height: 2.5rem;
-                           </x-slot:styles>
+                            <x-slot:styles>
+                                height: 2.5rem;
+                            </x-slot:styles>
                         </x-application-logo>
                     </a>
                 </div>
@@ -25,6 +25,9 @@
                     <x-nav-link :href="route('paths')" :active="request()->routeIs('paths')">
                         {{ __('Paths') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('users')" :active="request()->routeIs('users')">
+                        {{ __('Users') }}
+                    </x-nav-link>
 
                 </div>
             </div>
@@ -35,9 +38,8 @@
                     <x-slot name="trigger">
                         <button
                             class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            @if(isset(Auth::user()->firstname) && isset(Auth::user()->lastname))
-                                <div>{{ Auth::user()->getDisplayName() }}</div>
-                            @endif
+
+                            <div>{{ Auth::user()->getDisplayName() }}</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
@@ -72,10 +74,14 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M4 6h16M4 12h16M4 18h16"/>
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                              stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                     </svg>
                 </button>
             </div>
--- routes/user.php
+++ routes/user.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::prefix('/users')->group(function () {
+        Route::get('/', function () {
+            return view('users.list', ['users' => User::all()]);
+        })->name('users');
+
         Route::get('/me', function () {
             return redirect()->route('userProfile', ['user' => Auth::user()->uuid]);
         })->name('myProfile');
